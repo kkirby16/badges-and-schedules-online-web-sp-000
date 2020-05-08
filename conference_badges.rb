da8f3d854d5230d795c243638cd84ededc894b56
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-08 18:04:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
+  room_assignments = []
   attendees.each_with_index do |attendee, index| 
     "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
